9dffc309cf04ac3c325c75baf247c7f862ea68b7
Mariam Joan <royalton@gmail.com>
2018-02-25 01:17:27 -0800

    jackson

--- routes/routes.js
+++ routes/routes.js
@@ -1,70 +1,77 @@
 const express = require("express");
-const app = express();
+const router = express.Router();
 const connection = require("../database/connection.js")
 
-module.exports = function(app) {
-
-    app.get("/", function(req, res) {
-        connection.query("SELECT * FROM goals;", function(err, data) {
-            if (err) {
-                return res.status(500);
-            }
-            res.render("index", { goals: data });
-        });
+router.get("/", function(req, res) {
+    connection.query("SELECT * FROM goals;", function(err, data) {
+        if (err) {
+            return res.status(500);
+            console.log("err", err)
+        }
+        res.render("index", { goals: data });
+        console.log("routes working for home")
     });
+});
 
 
-    // CREATE
-    app.post("/goal", function(req, res) {
-        connection.query("INSERT INTO goals (goal) VALUES (?)", [req.body.goal], function(err, result) {
-            if (err) {
-                return res.status(500);
-            }
-            console.log("create goal set:", req.body.goal)
-            res.json({ id: result.insertId });
-            console.log({ id: result.insertId });
-        });
+// CREATE
+router.post("/goal", function(req, res) {
+    connection.query("INSERT INTO goals (goal) VALUES (?)", [req.body.goal], function(err, result) {
+        if (err) {
+            return res.status(500);
+            console.log("err", err)
+        }
+        console.log("create goal set:", req.body.goal)
+        res.json({ id: result.insertId });
+        console.log({ id: result.insertId });
     });
+});
 
 
-    // GET ALL
-    app.get("/goals", function(req, res) {
-        connection.query("SELECT * FROM goals;", function(err, data) {
-            if (err) {
-                return res.status(500);
-            }
-            res.json(data);
-        });
+// GET ALL
+router.get("/goals", function(req, res) {
+    connection.query("SELECT * FROM goals;", function(err, data) {
+        if (err) {
+            return res.status(500);
+            console.log("err", err)
+        }
+        console.log("routes working for get all")
+        res.json(data);
     });
+});
 
 
-    // UPDATE
-    app.put("/goal/:id", function(req, res) {
-        connection.query("UPDATE goals SET goals = ? WHERE id = ?", [req.body.plan, req.params.id], function(err, result) {
-            if (err) {
-                // If an error occurred, send a generic server faliure
-                return res.status(500);
-            } else if (result.changedRows == 0) {
-                // If no rows were changed, then the ID must not exist, so 404
-                return res.status(404);
-            } else {
-                res.status(200);
-            }
-        });
+// UPDATE
+router.put("/goal/:id", function(req, res) {
+    connection.query("UPDATE goals SET goals = ? WHERE id = ?", [req.body.goal, req.params.id], function(err, result) {
+        if (err) {
+            // If an error occurred, send a generic server faliure
+            return res.status(500);
+            console.log("err", err)
+        } else if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404);
+            console.log("err", err)
+        } else {
+            res.status(200);
+            console.log("route working at update", req.body.goal)
+        }
     });
+});
 
 
-    // DELETE
-    app.delete("/goal/:id", function(req, res) {
-        connection.query("DELETE FROM goals WHERE id = ?", [req.params.id], function(err, result) {
-            if (err) {
-                return res.status(500);
-            } else if (result.affectedRows == 0) {
-                return res.status(404);
-            } else {
-                res.status(200);
-            }
-        });
-    })
+// DELETE
+router.delete("/goal/:id", function(req, res) {
+    connection.query("DELETE FROM goals WHERE id = ?", [req.params.id], function(err, result) {
+        if (err) {
+            return res.status(500);
+        } else if (result.affectedRows == 0) {
+            return res.status(404);
+        } else {
+            res.status(200);
+        }
+    });
+})
 
-}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const app = express();
 
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
+
 const routes = require("./routes/routes.js");
 
-const PORT = process.env.PORT || 3306;
+
+const PORT = process.env.PORT || 8080;
 
 const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
