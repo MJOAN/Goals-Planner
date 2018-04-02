a7aadeb9178ad1bb8188f281ebf2adeb0bd4fb9a
Mariam Joan <royalton@gmail.com>
2018-04-02 16:58:58 -0700

    engine handlebars views

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 const express = require("express");
-const app = express();
-
 const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
-
+const app = express();
 const PORT = process.env.PORT || 3306;
-
-const routes = require("./routes/routes.js");
+const path = require("path");
 
 app.use(express.static(__dirname + "/public"));
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+const exphbs = require('express-handlebars');
+const viewsPath = path.join(__dirname, 'views');
+
+app.engine("handlebars", exphbs({ 
+	defaultLayout: "main", 
+	layoutsDir: viewsPath + "/layouts"
+}));
+
+app.set("view engine", "handlebars");
+
+const routes = require("./routes/routes.js");
 app.use("/", routes);
 
 require("./database/config.json");
 require("./database/connection.js");
-
-
-console.log("this server.js runs");
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
