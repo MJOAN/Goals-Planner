259bd97a6f2ff9e16a586a592f096c8f95510674
Mariam Joan <royalton@gmail.com>
2018-04-01 23:26:19 -0400

    connection

--- database/connection.js
+++ database/connection.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
-const connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "goals_db"
-    })
-}    
+// Set up our connection information
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+// Connect to the database
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-
+// Export connection
 module.exports = connection;
+
+
