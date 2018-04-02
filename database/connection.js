8924ea48970daf90f40689b9c6b38195be3613c2
Mariam Joan <royalton@gmail.com>
2018-04-02 00:12:28 -0400

    update mysq

--- database/connection.js
+++ database/connection.js
@@ -1,33 +1,23 @@
-// const mysql = require("mysql");
+const mysql = require('mysql');
+const connection;
 
-// // Set up our connection information
-// const connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: 'goals_db'
+  });
+};
 
-// // Connect to the database
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// // Export connection
-// module.exports = connection;
-
-
-
-
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
-
 
 module.exports = connection;
