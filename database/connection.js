37256c00c631bfc8bd7584e2d97cd38153d70b37
Mariam Joan <royalton@gmail.com>
2018-04-02 00:15:49 -0400

    update jaws

--- database/connection.js
+++ database/connection.js
@@ -1,23 +1,34 @@
-const mysql = require('mysql');
-const connection;
+const mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: 'goals_db'
-  });
-};
+const source = {
+    // localhost
+    localhost: {
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "goals_db"
+    },
+
+    // jawsDB
+    jawsDB: {
+        port: 3306,
+        host: "t89yihg12rw77y6f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "x2nslg2srpk4dbg1",
+        password: "riw9wzi0dkdf9l7e",
+        database: "dyacgh3cxp6tc4o8"
+    }
+    
+const connection = mysql.createConnection(source.jawsDB);
+// const connection = mysql.createConnection(process.env.JAWSDB_URL);
+connection.connect();
 
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
